51b4091ddfbce9bf06192b5890aacb61f1381a79
devsmish <smyshynskyi@gmail.com>
2026-07-08 13:35:11 +0200

    feat: implement custom JWT authentication layer with token_version check

--- config/settings.py
+++ config/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'apps.security.authentication.CustomJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
