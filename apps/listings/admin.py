e3f3106fc5cd6bd5c21b5ab1c54ca88eb83ee6ec
devsmish <smyshynskyi@gmail.com>
2026-07-11 21:21:00 +0200

    refactoring of the codebase and app listings structure

--- apps/listings/admin.py
+++ apps/listings/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-from apps.listings.models import Amenity, Listing, Room
+from apps.listings.models import Address, Amenity, Listing, ListingAmenity, Room
 
 
 class RoomInline(admin.TabularInline):
     model = Room
     extra = 1
     fields = ("room_type", "name", "max_guests")
+
+
+class ListingAmenityInline(admin.TabularInline):
+    model = ListingAmenity
+    extra = 1
 
 
 @admin.register(Amenity)
@@ -18,34 +23,72 @@ class AmenityAdmin(admin.ModelAdmin):
     readonly_fields = ("group",)
 
 
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("city", "street", "house_number", "postal_code", "region")
+    list_filter = ("country", "region", "city")
+    search_fields = ("city", "street", "postal_code")
+
+
 @admin.register(Listing)
 class ListingAdmin(admin.ModelAdmin):
     list_display = (
         "title",
         "type",
         "status",
+        "is_active",
         "owner",
-        "price_per_night",
+        "current_price",
         "max_guests",
+        "views_count",
         "created_at",
     )
 
-    list_filter = ("type", "status", "rental_type", "meal_type", "created_at")
+    list_filter = (
+        "type",
+        "status",
+        "is_active",
+        "rental_type",
+        "meal_type",
+        "created_at",
+    )
 
-    search_fields = ("title", "owner__username", "owner__email")
+    search_fields = ("title", "owner__email", "owner__nickname")
 
-    filter_horizontal = ("amenities",)
+    inlines = [RoomInline, ListingAmenityInline]
 
-    inlines = [RoomInline]
+    readonly_fields = ("views_count", "rating_avg", "reviews_count")
 
     fieldsets = (
-        (None, {"fields": ("title", "description", "owner", "status", "type")}),
-        (_("Location & Capacity"), {"fields": ("latitude", "longitude", "max_guests")}),
+        (
+            None,
+            {
+                "fields": (
+                    "title",
+                    "description",
+                    "owner",
+                    "status",
+                    "type",
+                    "is_active",
+                )
+            },
+        ),
+        (
+            _("Location & Capacity"),
+            {"fields": ("address", "max_guests", "rooms_count")},
+        ),
         (
             _("Pricing & Rules"),
-            {"fields": ("price_per_night", "rental_type", "meal_type")},
+            {"fields": ("current_price", "rental_type", "meal_type")},
         ),
-        (_("Amenities & Features"), {"fields": ("amenities",)}),
+        (
+            _("Deposit"),
+            {"fields": ("deposit_required", "deposit_percent", "deposit_refundable")},
+        ),
+        (
+            _("Analytics"),
+            {"fields": ("views_count", "rating_avg", "reviews_count")},
+        ),
     )
 
 
