57f5a373a0d693828f0c6c9764a134a163bf63bd
devsmish <smyshynskyi@gmail.com>
2026-07-13 08:42:22 +0200

    delete old app pricing

--- apps_pricing/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PricingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"  # CHANGED: было DEFAULT_AUTO_FIELD — Django не читает
-    name = "apps.pricing"
-    verbose_name = "Pricing"  # NEW
